34dde502c278c561ec90d661a65caa77a1d1cc38
chiennguyen <chiennguyen44@yahoo.com>
2014-04-08 14:26:32 -0700

    Initial Commit

---
+++ DataStructureTests/DataStructureTests.m
@@ -0,0 +1,34 @@
+//
+//  DataStructureTests.m
+//  DataStructureTests
+//
+//  Created by CHIEN NGUYEN on 4/8/14.
+//  Copyright (c) 2014 CHIEN NGUYEN. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DataStructureTests : XCTestCase
+
+@end
+
+@implementation DataStructureTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
